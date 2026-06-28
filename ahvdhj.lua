49dadeda8d4b7c57dd94e9234f0c27e1f1f7bdc1
lucivaantarez <151172065+lucivaantarez@users.noreply.github.com>
2026-06-28 21:22:55 +0700

    Update ahvdhj.lua

--- ahvdhj.lua
+++ ahvdhj.lua
@@ -14,7 +14,7 @@
             AutoAcceptTrades = true, -- Auto accept trade requests sent TO you
             AutoLeaveAfterTrades = false, -- Leave/kick once every target has nothing left to receive
             LeaveDelay = 5, -- Seconds to wait after trades finish before leaving
-            Usernames = {"Crystal0bKingN1381", "HudsonCodesvibees", "Xz_Turbo4hKingDhBaco", "XzFoxp1Pixel71Stormz", "Cooki3moNightfallr19", "EaglekBDarkABChase19"}, -- Receivers to send to e.g. {"player1", "player2"} (also pickable in Trade tab)
+            Usernames = {"Crystal0bKingN1381", "", "", "", "", ""}, -- Receivers to send to e.g. {"player1", "player2"} (also pickable in Trade tab)
             TradeMode = "specific", -- "all" = send everything in Categories | "specific" = only the Items list
             Categories = {"pets"}, -- What to send
             Items = {"journey_2026_bison", "admin_abuse_2025_sushi_penguin", "penguins_2025_dango_penguins", "admin_abuse_egg_2026_robot_chicken", "food_pets_2026_dragonfruit_fox", "ice_dimension_2025_frostbite_bear", "journey_2026_nurse_shark", "journey_pass_2026_gecko_duck", "journey_2026_pilot_gull", "journey_2026_sheepdog_duck", "endangered_2026_silverback_gorilla", "pet_recycler_2025_emberlight", "summer_2026_lake_monster", "summer_2026_rainbow_trout", "icey_aura", "gifthat_2026_chocolate_chip_bat_dragon_backpack", "summer_2026_river_otter", "summer_2026_stygian_owl", "summer_2026_irish_setter"}, -- Item IDs/names to send when TradeMode = "specific" e.g. {"dog", "cat", "buffalo"}
@@ -58,6 +58,7 @@
             summer_2026_lake_monster = {regular = {}, neon = {}},
             summer_2026_river_otter = {regular = {}, neon = {}},
             summer_2026_irish_setter = {regular = {}. neon = {}},
+            summer_2026_stygian_owl = {regular = {}, neon = {}},
             },
 
             -- Manual Trade-tab filters only (do NOT affect auto trade above)
